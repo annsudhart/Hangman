4fac6cae15ebd4228a2c86e3318b4b55f4f69047
annsudhart <32887362+annsudhart@users.noreply.github.com>
2018-12-17 09:49:37 -0800

    generateWord determines a word for player to guess
    generateWord determines a word for player to guess depending on difficulty selected

--- Hangman.java
+++ Hangman.java
@@ -10,11 +10,40 @@ public class Hangman {
 	private static String WELCOME_MESSAGE = "Welcome to Hangman!";
 	private static String CHOOSE_DIFFICULTY = "Choose a difficulty: [e]asy [m]edium [h]ard";
 	
+	private String ILLEGAL_DIFFICULTY_EXCEPTION = "Error: Illegal difficulty inputted. Inputs: [e]asy [m]edium [h]ard";
+	
 	private String difficulty = MED_STRING;
+	private String hangmanWord = "";
 	
 	
 	public Hangman(String difficulty) {
-		
+		this.difficulty = difficulty;
+		pickWord();
+	}
+	
+	private void pickWord() {
+		// if easy, choose from list of  <= 6 words
+		// if medium, choose from list of 6 <= x <= 12
+		// if hard, choose from list of x >= 12
+		int minLength;
+		int maxLength;
+		switch(difficulty) {
+			case EASY_STRING:
+				minLength = -1;
+				maxLength = 6;
+				break;
+			case MED_STRING:
+				minLength = 6;
+				maxLength = 12;
+				break;
+			case HARD_STRING:
+				minLength = 12;
+				maxLength = -1;
+				break;
+			default:
+				System.out.println(ILLEGAL_DIFFICULTY_EXCEPTION);
+		}
+		// TODO: parse enable1.txt for a word
 	}
 	
 	public static void main(String[] args) {
